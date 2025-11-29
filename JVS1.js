504cc448f3d857cd8327634c58d8e2b94c4a53e2
IvoIsmael <ismael.av0.8@gmail.com>
2025-11-28 21:57:37 -0400

    Adicione animaciones y bienvenida

--- JVS1.js
+++ JVS1.js
@@ -1,2 +1,5 @@
 console.log("Bienvenidos a mi portafolio");
 alert("Gracias por visitar mi pagina");
+document.querySelector('h1').addEventListener('click', () => {
+    alert('Gracias por visitar mi pagina')
+});
