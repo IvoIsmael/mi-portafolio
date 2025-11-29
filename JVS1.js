e7eefecae86d0e48f53ed122f741ceb390412660
IvoIsmael <ismael.av0.8@gmail.com>
2025-11-28 21:50:14 -0400

    Se agrego la seccion habilidades

--- JVS1.js
+++ JVS1.js
@@ -1,16 +1,2 @@
-//Definir modulo principal
-var app = angular.module('miApp', []);
-//controlador 1: ejemplo simple sin variable
-app.controller('miCtrl', function($scope){
-    $scope.nombre = "Ivo";
-});
-//Controlador 2: lista dinamica
-app.contoller('listaCtrl', function($scope){
-    $scope.tareas = ['estudar', 'comer', 'dormir'];
-    $scope.agregar = function(){
-        if($scope.nuevaTarea && $scope.nuevaTarea.trim() !== ""){
-            $scope.tareas.push($scope.nuevaTarea);
-            $scope.nuevaTarea = '';
-        }
-    };
-})
+console.log("Bienvenidos a mi portafolio");
+alert("Gracias por visitar mi pagina");
