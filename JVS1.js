bf202e74962540dd826ce5ded010ae6cfe293242
IvoIsmael <ismael.av0.8@gmail.com>
2025-11-29 08:35:18 -0400

    Estructura mejorada

--- JVS1.js
+++ JVS1.js
@@ -1,5 +1,4 @@
-console.log("Bienvenidos a mi portafolio");
-alert("Gracias por visitar mi pagina");
+
 document.querySelector('h1').addEventListener('click', () => {
-    alert('Gracias por visitar mi pagina')
+    alert('Hola soy Ivo')
 });
